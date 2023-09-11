6b123da48fd2c896d3dacfecd0ad3af86830c8a6
hambone-the-Great <marcus.schuff@gmail.com>
2023-09-11 11:51:55 -0700

    updated assembly info. Trying to publish.

--- PDFProtector/Properties/AssemblyInfo.cs
+++ PDFProtector/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PDFProtector")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PDF Protector")]
+[assembly: AssemblyDescription("An open source program that password protects PDF documents")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PDFProtector")]
+[assembly: AssemblyCompany("Schuff Software Company")]
+[assembly: AssemblyProduct("PDF Protector")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1acbbd61-5fe9-4210-bb0d-ee430329c32e")]
